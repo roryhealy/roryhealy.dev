cb19ce522e85f26f473392591da4054322432981
Rory Healy <roryihealy@outlook.com>
2023-07-22 07:11:55 +1000

    Add links to icons

--- components/icons/icons.tsx
+++ components/icons/icons.tsx
@@ -16,14 +16,32 @@ export default function IconRow({ icons, className, size }: IIconRow) {
   const defaultIconSize: number = 100;
 
   function iconToImage(icon: Icons) {
-    return (
-      <Image
-        src={`images/${icon}.svg`}
-        alt={`${icon.toUpperCase()} Logo`}
-        width={size ? size : defaultIconSize}
-        height={size ? size : defaultIconSize}
-      />
-    );
+    switch (icon) {
+      case Icons.Gitea:
+        return (
+          <a href='https://git.roryhealy.dev'>
+            <Image
+              src='images/gitea.svg'
+              alt='Gitea Logo'
+              width={size ? size : defaultIconSize}
+              height={size ? size : defaultIconSize}
+            />
+          </a>
+        );
+      case Icons.Github:
+        return (
+          <a href='https://github.com/roryhealy'>
+            <Image
+              src='images/github.svg'
+              alt='Gitea Logo'
+              width={size ? size : defaultIconSize}
+              height={size ? size : defaultIconSize}
+            />
+          </a>
+        );
+      default:
+        return;
+    }
   }
 
   return <div className={`flex gap-x-10 ${className}`}>{icons.map((icon) => iconToImage(icon))}</div>;
