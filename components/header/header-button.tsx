4695f392e32fc50e7af0944ee1296caf004263cb
Rory Healy <roryihealy@outlook.com>
2023-07-22 07:27:12 +1000

    Add routes to header buttons

--- components/header/header-button.tsx
+++ components/header/header-button.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
 
 interface IHeaderButton {
   displayText: string;
+  link: string;
 }
 
-export default function HeaderButton({ displayText }: IHeaderButton) {
+export default function HeaderButton({ displayText, link }: IHeaderButton) {
   return (
-    <div className='grid grid-rows-2 justify-center gap-y-1 justify-items-center'>
-      <p className='text-center text-4xl text-custom-white'>{displayText}</p>
-      <div className='bg-custom-blue-1 h-1 rounded w-full hover:w-1/2 transition-all duration-300 ease-in-out'></div>
-    </div>
+    <Link href={link} >
+      <div className='grid grid-rows-2 justify-center gap-y-1 justify-items-center'>
+        <p className='text-center text-4xl text-custom-white'>{displayText}</p>
+        <div className='bg-custom-blue-1 h-1 rounded w-full hover:w-1/2 transition-all duration-300 ease-in-out'></div>
+      </div>
+    </Link>
   );
 }
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -6,10 +6,10 @@ export default function Header() {
   return (
     <header className='grid grid-cols-10 px-14 pt-8'>
       <p className='uppercase col-span-6 text-left text-4xl text-custom-white'>Rory Healy</p>
-      <HeaderButton displayText='Home'/>
-      <HeaderButton displayText='Projects'/>
-      <HeaderButton displayText='About'/>
-      <HeaderButton displayText='Contact'/>
+      <HeaderButton displayText='Home' link='/' />
+      <HeaderButton displayText='Projects' link='/projects' />
+      <HeaderButton displayText='About' link='/about' />
+      <HeaderButton displayText='Contact' link='/contact' />
     </header>
   );
 }
